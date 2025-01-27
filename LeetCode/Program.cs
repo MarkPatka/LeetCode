ca2bead0d5142b64edc5cf0fb483f2ffab549039
Mark Patka <mark.kaz.1998@gmail.com>
2025-01-27 21:15:24 +0300

    Completes Task 26 Removing Duplicates From Sorted Array

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,23 @@
 ﻿using LeetCode.Top150Interview.MergeSortedArray;
+using LeetCode.Top150Interview.RemoveDuplicatesSortedArray_26;
+using LeetCode.Top150Interview.RemoveElement_27;
 
 
-MergeSortedArray msa = new ();
-int[] nums1 = [1, 2, 2, 0, 0, 0];
-int m = 3;
-int[] nums2 = [3, 8, 9];
-int n = 3;
-msa.Merge3(nums1, m, nums2, n);
+//MergeSortedArray msa = new ();
+//int[] nums1 = [1, 2, 2, 0, 0, 0];
+//int m = 3;
+//int[] nums2 = [3, 8, 9];
+//int n = 3;
+//msa.Merge3(nums1, m, nums2, n);
+
+
+//RemoveElement rmv = new RemoveElement();
+//int[] nums = [3, 2, 2, 3];
+//int val = 3;
+//rmv.Remove2(nums, val);
+//Console.ReadLine();
+
+RemoveDuplicates rmv = new();
+int[] nums = [3, 5, 8, 12, 4, 8, 3, 1, 2, 12];
+rmv.Remove1(nums);
 Console.ReadLine();
---
+++ LeetCode/Top150Interview/RemoveDuplicatesSortedArray_26/RemoveDuplicates.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Top150Interview.RemoveDuplicatesSortedArray_26;
+
+public class RemoveDuplicates
+{
+    public int Remove1(int[] nums)
+    {
+        if (nums.Length == 0) { return 0; }
+
+        int k = 0;
+        List<int> unique = [];
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            bool isDuplicate = false;
+            for (int j = 0; j < unique.Count; j++)
+            {
+                if (nums[i] == unique[j])
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicate)
+            {
+                unique.Add(nums[i]);
+                k++;
+            }
+        }
+
+        for (int i = 0; i < k; i++) 
+        {
+            nums[i] = unique[i];
+        }
+        return k;
+    }
+
+    public int Remove2(int[] nums)
+    {
+        var nextDistinctElementIndex = 1;
+
+        for (var currentIndex = 1; currentIndex < nums.Length; currentIndex++)
+        {
+            var previousIndex = currentIndex - 1;
+            if (nums[currentIndex] != nums[previousIndex])
+            {
+                nums[nextDistinctElementIndex] = nums[currentIndex];
+                nextDistinctElementIndex++;
+            }
+        }
+
+        return nextDistinctElementIndex;
+    }
+}
