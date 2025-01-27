bcd54e1c1e3e6f180412f854fe3fbe423f03f983
Mark Patka <mark.kaz.1998@gmail.com>
2025-01-27 19:50:10 +0300

    Completes Task 27 Removing Elements

--- LeetCode/Top150Interview/RemoveElement_27/RemoveElement.cs
+++ LeetCode/Top150Interview/RemoveElement_27/RemoveElement.cs
@@ -1,6 +1,44 @@
-﻿namespace LeetCode.Top150Interview.RemoveElement_27;
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace LeetCode.Top150Interview.RemoveElement_27;
 
 public class RemoveElement
 {
+    /// <summary>
+    /// WRONG due to constraint: num[i] >= 0 
+    /// </summary>
+    public int Remove1(int[] nums, int val)
+    {
+        int len = nums.Length;
+        if (len == 0) return 0;
+        
+        int removedCnt = 0;
+        int idx = 0;
+        while (idx < len)
+        {
+            if (nums[idx] == val)
+            {
+                nums[idx] = -1;
+                removedCnt++;
+                (nums[idx], nums[^removedCnt]) = (nums[^removedCnt], nums[idx]);
+                continue;
+            }
+            idx++;
+        }
+        return len - removedCnt;
+    }
 
+    public int Remove2(int[] nums, int val)
+    {
+        int index = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
 }
