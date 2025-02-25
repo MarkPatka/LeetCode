446ea70490e0e0ddf56e085f3aff1243ff80d18e
usr <usr@PC1>
2025-02-25 17:09:20 +0300

    Completes Task 274

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode.Top150Interview.BestTimeToBuyAndSellStock_122;
+using LeetCode.Top150Interview.HIndex_274;
 using LeetCode.Top150Interview.JumpGame_55;
 using LeetCode.Top150Interview.JumpGame2_45;
 using LeetCode.Top150Interview.Majority_Element_169;
@@ -8,99 +9,12 @@ using LeetCode.Top150Interview.RemoveElement_27;
 using LeetCode.Top150Interview.RotateArray_189;
 
 
-//MergeSortedArray msa = new ();
-//int[] nums1 = [1, 2, 2, 0, 0, 0];
-//int m = 3;
-//int[] nums2 = [3, 8, 9];
-//int n = 3;
-//msa.Merge3(nums1, m, nums2, n);
+HIndex hidx = new();
+int[] arr1 = [3, 0, 6, 1, 5, 3, 2];
+int[] arr2 = [1, 3, 1];
+int[] arr3 = [100];
+var res1 = hidx.GetHIndex(arr1);
+var res2 = hidx.GetHIndex(arr2);
+var res3 = hidx.GetHIndex(arr3);
 
-
-//RemoveElement rmv = new RemoveElement();
-//int[] nums = [3, 2, 2, 3];
-//int val = 3;
-//rmv.Remove2(nums, val);
-//Console.ReadLine();
-
-//LeetCode.Top150Interview.RemoveDuplicatesSortedArray_26.RemoveDuplicates rmv = new();
-//int[] nums = [3, 5, 8, 12, 4, 8, 3, 1, 2, 12];
-//rmv.Remove2(nums);
-//Console.ReadLine();
-
-//LeetCode.Top150Interview.RemoveDuplicatesSortedArray_80.RemoveDuplicates rmv = new();
-//[1, 1, 1, 2, 2, 3]
-//[1,2,2]
-//[1,1,1]
-//[0,0,1,1,1,1,2,3,3]
-//int[] nums = [1, 1, 1, 2, 2, 3];
-//int res = rmv.Remove1(nums);
-//Console.ReadLine();
-
-//MajorityElement majorityElement = new();
-//int[] nums1 = [3, 2, 3];
-//int[] nums2 = [2, 2, 1, 1, 1, 2, 2];
-//majorityElement.MajorityElement1(nums1);
-//majorityElement.MajorityElement1(nums2);
-//Console.ReadLine();
-
-//RotateArray ra = new();
-//int[] nums = [1, 2, 3, 4, 5, 6, 7];
-//int k = 3;
-//ra.Rotate(nums, k);
-//Console.ReadLine();
-
-//BestTimeToBuyAndSellStock bs = new();
-//int[] prices1 = [7, 1, 5, 3, 6, 4];
-//int[] prices2 = [1, 2, 3, 4, 5];
-//int[] prices3 = [7, 6, 4, 3, 1];
-
-//int res1 = bs.MaxProfit(prices1);
-//int res2 = bs.MaxProfit(prices2);
-//int res3 = bs.MaxProfit(prices3);
-
-//JumpGame jg = new JumpGame();
-//int[] jumps1 = [2, 3, 1, 1, 4];
-//int[] jumps2 = [2, 5, 0, 0];
-//int[] jumps3 = [2, 0];
-//int[] jumps4 = [3, 2, 1, 0, 4];
-//int[] jumps5 = [0, 1];
-//int[] jumps6 = [1,1,1,0];
-//int[] jumps7 = [0];
-//int[] jumps8 = [1];
-//int[] jumps9 = [1, 2, 0, 1];
-//int[] jumps10 = [3, 0, 8, 2, 0, 0, 1];
-//bool res1  = jg.CanJump_1(jumps1);
-//bool res2  = jg.CanJump_1(jumps2);
-//bool res3  = jg.CanJump_1(jumps3);
-//bool res4  = jg.CanJump_1(jumps4);
-//bool res5  = jg.CanJump_1(jumps5);
-//bool res6  = jg.CanJump_1(jumps6);
-//bool res7  = jg.CanJump_1(jumps7);
-//bool res8  = jg.CanJump_1(jumps8);
-//bool res9  = jg.CanJump_1(jumps9);
-//bool res10 = jg.CanJump_1(jumps10);
-
-
-JumpGame2 jg2 = new();
-int[] nums1 = [2, 3, 1, 1, 4];
-int[] nums2 = [2, 5, 0, 0];
-int[] nums3 = [2, 0];
-int[] nums4 = [3, 2, 1, 0, 4];
-int[] nums5 = [0, 1];
-int[] nums6 = [1, 1, 1, 0];
-int[] nums7 = [0];
-int[] nums8 = [1];
-int[] nums9 = [1, 2, 0, 1];
-int[] nums10 = [3, 0, 8, 2, 0, 0, 1];
-
-int res1 = jg2.Jump(nums1);
-int res2 = jg2.Jump(nums2);
-int res3 = jg2.Jump(nums3);
-int res4 = jg2.Jump(nums4);
-int res5 = jg2.Jump(nums5);
-int res6 = jg2.Jump(nums6);
-int res7 = jg2.Jump(nums7);
-int res8 = jg2.Jump(nums8);
-int res9 = jg2.Jump(nums9);
-int res10 = jg2.Jump(nums10);
 Console.ReadLine();
