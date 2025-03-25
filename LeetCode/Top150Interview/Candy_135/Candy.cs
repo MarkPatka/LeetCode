6b3c0e528f2d4e1fda2b90c56802c7824bf58191
usr <usr@PC1>
2025-03-25 16:42:35 +0300

    Completes Task 135 Candy

--- LeetCode/Top150Interview/Candy_135/Candy.cs
+++ LeetCode/Top150Interview/Candy_135/Candy.cs
@@ -4,16 +4,11 @@ namespace LeetCode.Top150Interview.Candy_135;
 
 public partial class Solution
 {
-    //Input: ratings = [1, 0, 2]
-    //Output: 5 =>     [2, 1, 2]
- 
-    //Input: ratings = [1, 2, 2]
-    //Output: 4 =>     [1, 2, 1]
+    /// It`s Okay
     public int Candy(int[] ratings)
     {
         int l = ratings.Length;
         if (l == 1) return 1;
-        if (l == 2) return ratings[0] == ratings[1] ? 2 : 3;
         
         int[] dist = new int[l];
         Array.Fill(dist, 1);
@@ -22,44 +17,58 @@ public partial class Solution
         {
             if (ratings[i] > ratings[i - 1])
             {
-                dist[i]++;
+                dist[i] = dist[i - 1] + 1;
             }
         }
+
         for (int i = l - 2; i >= 0; i--)
         {
             if (ratings[i] > ratings[i + 1])
-            {
-                dist[i]++;
+            {                
+                dist[i] = Math.Max(dist[i], dist[i + 1] + 1);
             }
-        }
-
-        if (ratings[0] > ratings[1]) dist[0]++;
-        if (ratings[^2] < ratings[^1]) dist[^1]++;
-
-        for (int i = 0; i < l; i++)
-        {
-            if (dist[i] == 0) dist[i]++;
         }
 
         return dist.Sum();
     }
 
-    private (int l, int r) ProcessBorders(int left, int right)
+    /// this one has best performance
+    public int Candy2(int[] ratings)
     {
-        (int l, int r) = (0, 0);
+        int l = ratings.Length;
+        if (l == 1) return 1;
 
-        if (left != right)
+        int totalCandies = 1;
+        int up = 1, down = 0, peak = 1;
+
+        for (int i = 1; i < l; i++)
         {
-            if (left > right)
+            if (ratings[i] > ratings[i - 1])
             {
-                l++;
+                up++;
+                down = 0;
+                peak = up;
+                totalCandies += up;
+            }
+            else if (ratings[i] < ratings[i - 1])
+            {
+                down++;
+                up = 1;
+                totalCandies += down;
+                if (peak <= down)
+                {
+                    totalCandies++;
+                }
             }
             else
             {
-                r++;
+                up = 1;
+                down = 0;
+                peak = 1;
+                totalCandies += 1;
             }
         }
-        return (l, r);
 
+        return totalCandies;
     }
 }
