1d24953fa17ff782b86a32bf07be609c77219156
usr <usr@PC1>
2025-03-31 13:20:00 +0300

    Trying to Solve Trap Task.

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,7 +4,9 @@ using LeetCode.Top150Interview.TrappingRainWater_42;
 Console.WriteLine("LeetCode");
 
 int[] input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+int[] input2 = [2, 0, 1, 0, 1, 3, 1, 0, 0, 1, 2, 0, 0, 2];
+
 Solution solution = new Solution();
 
-solution.Trap(input1);
+solution.Trap_2(input2);
 
--- LeetCode/Top150Interview/TrappingRainWater_42/TrappingRainWater.cs
+++ LeetCode/Top150Interview/TrappingRainWater_42/TrappingRainWater.cs
@@ -43,4 +43,42 @@ public partial class Solution
         }
         return sum;
     }
+
+    public int Trap_2(int[] height)
+    {
+        int len = height.Length - 1;
+
+        int leftPosition = 0;
+        int rightPosition = 2;
+
+        int result = 0;
+        for (int i = 1; i < len; i++)
+        {
+            if ((height[leftPosition] > height[i]) && 
+                (height[leftPosition] <= height[rightPosition]))
+            {
+                result = 0;
+                int sq = height[leftPosition] * (i - leftPosition);
+                int extract = height[(leftPosition + 1) .. rightPosition].Sum();
+                result += sq - extract;
+
+                leftPosition = rightPosition;
+                rightPosition++;
+                continue;
+            }
+
+            if (height[i] < height[rightPosition] && 
+                height[rightPosition] < height[leftPosition])
+            {
+                result += height[rightPosition] - height[i];
+            }
+
+            // If height[i] = 0 and If height[i + 1] ... = 0
+            // we need to consider the length of height[i] == 0
+
+            rightPosition++;
+        }
+
+        return result;
+    }
 }
