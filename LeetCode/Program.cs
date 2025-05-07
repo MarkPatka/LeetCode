a07a5e6910f5f8a2095013f063fdeac9e984f7a0
usr <usr@PC1>
2025-05-07 15:28:25 +0300

    Trapping rain water 42 solved

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,1 @@
-﻿
-using LeetCode.Top150Interview.TrappingRainWater_42;
-
-Console.WriteLine("LeetCode");
-
-int[] input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
-int[] input2 = [2, 0, 1, 0, 1, 3, 1, 0, 0, 1, 2, 0, 0, 2];
-
-Solution solution = new Solution();
-
-solution.Trap_2(input2);
-
+﻿Console.WriteLine("*** LeetCode PlayGroud ***");
--- LeetCode/Top150Interview/TrappingRainWater_42/TrappingRainWater.cs
+++ LeetCode/Top150Interview/TrappingRainWater_42/TrappingRainWater.cs
@@ -44,41 +44,96 @@ public partial class Solution
         return sum;
     }
 
+    /// <summary>
+    /// This one is better than first, 
+    /// but kinda brutforce and not optimal, 
+    /// despite it has been accepted
+    /// </summary>
     public int Trap_2(int[] height)
     {
-        int len = height.Length - 1;
+        int len = height.Length;
+        if (len <= 2) return 0;
 
-        int leftPosition = 0;
-        int rightPosition = 2;
+        int left = 0;
+        int right = 1;
 
-        int result = 0;
-        for (int i = 1; i < len; i++)
+        int trapped = 0;
+        for (; right < len; right++)
         {
-            if ((height[leftPosition] > height[i]) && 
-                (height[leftPosition] <= height[rightPosition]))
+            if (height[left] <= height[right]) { left++; continue; }
+            else if (right != len - 1)
             {
-                result = 0;
-                int sq = height[leftPosition] * (i - leftPosition);
-                int extract = height[(leftPosition + 1) .. rightPosition].Sum();
-                result += sq - extract;
+                int a = height[left] + 1;
+                int i = right;
+                int b = 0;
+                int r = -1;
+                do
+                {
+                    a--;
+                    for (; i < len; i++)
+                    {
+                        if (height[i] >= a)
+                        {
+                            b = i - left - 1;
+                            r = i;    
+                            break;
+                        }
+                    }
+                    i = right;
+                }
+                while (a > 0 && r < i);
+                if (r == -1) return trapped;
 
-                leftPosition = rightPosition;
-                rightPosition++;
-                continue;
+                int exclude = height[i..r].Sum();
+                trapped += (a * b) - exclude;
+                
+                right = r;
+                left = r;
             }
+        }
+        return trapped;
+    }
 
-            if (height[i] < height[rightPosition] && 
-                height[rightPosition] < height[leftPosition])
+    /// <summary>
+    /// The best one. 
+    /// It does Properly use two-pointers approach. 
+    /// And time complexity ~O(n), space complexity ~O(1)
+    /// </summary>
+    public int Trap_3(int[] height)
+    {
+        if (height == null || height.Length < 3)
+            return 0;
+
+        int left = 0;
+        int right = height.Length - 1;
+        
+        int leftMax = 0;
+        int rightMax = 0;
+        int result = 0;
+
+        while (left < right)
+        {
+            if (height[left] < height[right])
             {
-                result += height[rightPosition] - height[i];
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    result += leftMax - height[left];
+
+                left++;
             }
+            else
+            {
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    result += rightMax - height[right];
 
-            // If height[i] = 0 and If height[i + 1] ... = 0
-            // we need to consider the length of height[i] == 0
-
-            rightPosition++;
+                right--;
+            }
         }
 
         return result;
+
     }
 }
