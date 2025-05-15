9b47bfdefd1c4e951658a5ad30373108058004c0
usr <usr@PC1>
2025-05-15 11:46:23 +0300

    Completes Task 14 Longest Common Prefix

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,9 @@
-﻿using LeetCode.Top150Interview.LengthOfLastWord_58;
+﻿using LeetCode.Top150Interview.LongestCommonPrefix_14;
 
 Console.WriteLine("*** LeetCode PlayGroud ***");
 
 Solution solution = new Solution();
-int n1 = solution.LengthOfLastWord("Hello World");
-int n2 = solution.LengthOfLastWord("   fly me   to   the moon  ");
-int n3 = solution.LengthOfLastWord("luffy is still joyboy");
+string s = solution.LongestCommonPrefix(["cadebra", "cadeb", "cade", "cad"]);
+
 
 Console.ReadLine();
---
+++ LeetCode/Top150Interview/LongestCommonPrefix_14/LongestCommonPrefix.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Top150Interview.LongestCommonPrefix_14;
+
+public partial class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string minStr = strs[0];
+        if (strs.Length == 1) return minStr;
+
+        for (int i = 1; i < strs.Length; i++) 
+        {
+            if (string.IsNullOrEmpty(strs[i]))
+            {
+                return strs[i];
+            }
+
+            if (strs[i].Length < minStr.Length)
+            {
+                minStr = strs[i];
+            }
+        }
+
+        for (int i = minStr.Length - 1; i >= 0 ; i--)
+        {
+            string prefix = minStr[..(i + 1)];
+            int cnt = 0;
+            foreach (string str in strs)
+            {
+                if (!str.StartsWith(prefix))
+                {
+                    break;
+                }
+                cnt++;
+            }
+
+            if (cnt == strs.Length)
+            {
+                return prefix;
+            }
+        }
+
+        return "";
+    }
+}
