4efe86c5eaaca0c69751d4e5668747b891ff9d2a
usr <usr@PC1>
2025-05-16 08:44:26 +0300

    Completes Task 151 Longest Common Prefix

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,9 +1,7 @@
-﻿using LeetCode.Top150Interview.LongestCommonPrefix_14;
+﻿using LeetCode.Top150Interview.ReverseWordsInString_151;
 
 Console.WriteLine("*** LeetCode PlayGroud ***");
 
 Solution solution = new Solution();
-string s = solution.LongestCommonPrefix(["cadebra", "cadeb", "cade", "cad"]);
-
 
 Console.ReadLine();
