72832a936831663afd52a264ac9a1c89d6ae070d
Mark Patka <mark.kaz.1998@gmail.com>
2025-05-31 16:12:09 +0300

    Completes Task 68 TextJustification

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,12 @@
-﻿using LeetCode.Top150Interview.FindTheIndexOfTheFirstOccurenceInString_28;
+﻿using LeetCode.Top150Interview.TextJustification_68;
 
 Console.WriteLine("*** LeetCode PlayGroud ***");
 
 Solution solution = new Solution();
 
-int idx = solution.FindTheIndexOfTheFirstOccurenceInString("leetcode", "leeto");
+string[] words = ["What", "must", "be", "shall", "be."];
+int maxWidth = 5;
 
+var result = solution.FullJustify(words, maxWidth);
 
 Console.ReadLine();
