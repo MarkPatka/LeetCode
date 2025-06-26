5cfda2102995d7cd10b80acaef71f2f1b1cd1a7c
usr <usr@PC1>
2025-06-26 10:23:06 +0300

    Completes Task 209 Minimum Size Subarray Sum

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,15 +1,9 @@
-﻿using LeetCode.Top150Interview._3Sum_15;
+﻿using LeetCode.Top150Interview.MinimumSizeSubarraySum_209;
 
 Console.WriteLine("*** LeetCode PlayGroud ***");
 
 Solution solution = new();
 
-int[] nums1 = [-1, 0, 1, 2, -1, -4];
-int[] nums2 = [3, -2, 1, 0];
-int[] nums3 = [0, 0, 0];
 
-var result1 = solution.ThreeSum(nums1);
-var result2 = solution.ThreeSum(nums2);
-var result3 = solution.ThreeSum(nums3);
 
 Console.ReadLine();
