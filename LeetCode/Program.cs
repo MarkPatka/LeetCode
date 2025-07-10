3a9fa43061c0239446eabd0b38ca1c0144b1fb26
usr <usr@PC1>
2025-07-10 13:45:14 +0300

    Completes Task 47 Rotate Image

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,21 +1,13 @@
-﻿using LeetCode.Top150Interview.SpiralMatrix_54;
+﻿using LeetCode.Top150Interview.RotateImage_48;
 
 Console.WriteLine("*** LeetCode PlayGroud ***");
 
 Solution solution = new Solution();
 
-int[][] matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-int[][] matrix2 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+int[][] matrix1 = [[5, 2], [3, 7]];
 
-int[][] matrix3 = [[1],[2],[3],[4]];
-int[][] matrix4 = [[1, 2, 3 ,4]];
-int[][] matrix5 = [[1]];
+solution.Rotate(matrix1);
 
-//var res1 = solution.SpiralOrder(matrix1);
-//var res2 = solution.SpiralOrder(matrix2);
-var res3 = solution.SpiralOrder(matrix3);
-var res4 = solution.SpiralOrder(matrix4);
-var res5 = solution.SpiralOrder(matrix5);
 Console.ReadLine();
 
 
---
+++ LeetCode/Top150Interview/RotateImage_48/RotateImage.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Top150Interview.RotateImage_48;
+
+public partial class Solution
+{
+    public void Rotate(int[][] matrix)
+    {
+        int n = matrix.Length;
+
+        // transpose the matrix
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+
+                // Slower in practice then usage an explicit temp variable
+                // because XOR swaps involve multiple read/write operations
+                // and prevent compiler optimizations.
+                // 
+                // So its better to use tme variable to replace elements
+                // because of it’s likely faster in real-world execution
+                // due to better CPU optimization
+                matrix[i][j] = matrix[i][j] ^ matrix[j][i];
+                matrix[j][i] = matrix[i][j] ^ matrix[j][i];
+                matrix[i][j] = matrix[i][j] ^ matrix[j][i];
+            }
+        }
+
+        // reverse each line
+        foreach (var line in matrix)
+        {
+            Array.Reverse(line);
+        }
+    }
+}
