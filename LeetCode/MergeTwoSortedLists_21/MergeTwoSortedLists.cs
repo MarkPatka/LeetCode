1a979881fadfe673f426f2b213f77449bfcf8b6d
Mark Patka <mark.kaz.1998@gmail.com>
2025-11-21 13:31:42 +0300

    Completes Task 21 - Merge Two Sorted Lists

---
+++ LeetCode/MergeTwoSortedLists_21/MergeTwoSortedLists.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace LeetCode.MergeTwoSortedLists_21;
+
+public partial class Solution
+{
+    public ListNode MergeTwoLists(ListNode? list1, ListNode? list2)
+    {
+        var head = new ListNode();
+        var current = head;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                current!.next = list1;  // в качестве next добавляем узел с меньшим значением (list1 или list2)
+                list1 = list1.next;     // двигаем указатель list1 на следующий элемент
+            }
+            else
+            {
+                current!.next = list2;  // в качестве next добавляем узел с меньшим значением (val) (list1 или list2)
+                list2 = list2.next;     // двигаем указатель list2 на следующий элемент
+            }
+            current = current.next;     // сдвигаем указатель current на последний добавленный узел
+        }
+
+        current!.next = list1 is null ? list2 : list1; // если один из списков закончился, прикрепляем оставшуюся часть другого списка к current.next
+        return head.next!; // Возврат head.next — чтобы пропустить вспомогательный пустой узел
+    }
+}
+public class ListNode(int val = 0, ListNode? next = null)
+{
+    public int val = val;
+    public ListNode? next = next;
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,22 @@
-﻿using LeetCode.PalindromeNumber_9;
+﻿using LeetCode.MergeTwoSortedLists_21;
 
 Console.WriteLine("*** LeetCode PlayGroud ***");
 
 Solution solution = new Solution();
 
-var res = solution.IsPalindrome_2(1001);
+ListNode list1_3 = new(4, null);
+ListNode list1_2 = new(2, list1_3);
+ListNode list1_1 = new(1, list1_2);
+
+ListNode list2_3 = new(4, null);
+ListNode list2_2 = new(3, list2_3);
+ListNode list2_1 = new(1, list2_2);
+
+
+
+var res = solution.MergeTwoLists(list1_1, list2_1);
+
+
 
 
 Console.ReadLine();
