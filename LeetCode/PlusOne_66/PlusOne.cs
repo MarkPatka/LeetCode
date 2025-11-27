caec8d4fbf3b176fd8a28a543ab3ddbffab17e1c
Mark Patka <mark.kaz.1998@gmail.com>
2025-11-27 10:24:55 +0300

    Completes Task 66 - Plus One

---
+++ LeetCode/PlusOne_66/PlusOne.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.PlusOne_66;
+
+public partial class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        int i = digits.Length - 1;
+        while (digits[i] == 9 && i >= 0)
+        {
+            digits[i--] = 0;
+
+            if (i < 0) 
+            {
+                int[] result = new int[digits.Length + 1];
+                result[0] = 1;
+                return result;
+            }
+        }
+        
+        digits[i]++;
+
+        return digits;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,23 @@
-﻿using LeetCode.SearchInsertPosition_35;
+﻿using LeetCode.PlusOne_66;
 
 Console.WriteLine("*** LeetCode PlayGroud ***");
 
 Solution solution = new Solution();
 
-int[] nums = [1, 3, 5, 6];
+int[] nums0 = [0];
 
-var res = solution.SearchInsert(nums, 7);
+int[] nums1 = [9];
+int[] nums2 = [4, 3, 2, 1];
+int[] nums3 = [1, 3, 5, 9];
+int[] nums4 = [7, 9, 9, 9, 9];
+int[] nums5 = [1, 9, 0, 9, 9];
+var res1 = solution.PlusOne(nums1);
+var res2 = solution.PlusOne(nums2);
+var res3 = solution.PlusOne(nums3);
+var res4 = solution.PlusOne(nums4);
+var res5 = solution.PlusOne(nums5);
+var res6 = solution.PlusOne(nums0);
+
 
 
 
